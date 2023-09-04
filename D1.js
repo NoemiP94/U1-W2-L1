c9cc76751bd5dfc57c409cf99874950cca605997
Noemi Pusceddu <m_emi94@hotmail.it>
2023-09-04 17:29:30 +0200

    correction

--- D1.js
+++ D1.js
@@ -64,4 +64,4 @@ REGOLE
 // let name1 = "john"
 // let name2 = "John"
 // console.log(name1 === name2)
-// console.log(name1 === name2.toLowerCase())
+// console.log(name1.toLowerCase === name2.toLowerCase())
